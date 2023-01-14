744ab15950dfb39cac5348be974cfca4aef7a733
bazz <clement.f.bazz@gmail.com>
2023-01-14 07:56:21 +0100

    feat: Create NavBar and  basic styles for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useCalc } from "./calculator";
+import "./_App.scss";
 
 // JSX Component
 function App() {
   const { calcMethods } = useCalc()
   console.log(calcMethods.genSmallCaps())
   return (
-    <div className="App">
-    <h1>Password Generator</h1>
-    </div>
+    <main className="App">
+      <nav className="navBar">
+        <p className="logo">Clement <span className="dim">Bazuaye</span></p>
+      </nav>
+      <div className="generator">
+        <h1 className="title" >Password Generator</h1>
+
+        <section className="container ">
+          <p>Upper Section</p>
+        </section>
+
+        <section className="container ">
+          <p>Lower Section</p>
+        </section>
+      </div>
+    </main>
   );
 }
 
